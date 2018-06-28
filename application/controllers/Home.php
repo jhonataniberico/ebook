01cbb961fde95f97a2360a79e6cf6b12b85fcb32
Pedro Yturrizaga <blue2@MAC-BLUE2.local>
2018-06-28 17:29:12 -0500

    cambios

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	function __construct() {
-        parent::__construct();require_once APPPATH.'third_party/PHPExcel.php';
-        $this->excel = new PHPExcel(); 
+        parent::__construct();
         $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
         $this->load->model('M_usuario');
         $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
